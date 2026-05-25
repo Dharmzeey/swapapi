e5683534ce287b1d3b5eae34405324757d1f3320
unknown <olanrewajuadam2020@gmail.com>
2026-05-25 10:21:51 +0100

    entrypoint edited

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -2,4 +2,4 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-exec gunicorn --bind 0.0.0.0:8000 --workers 3 swap.wsgi:application
+exec gunicorn --bind 0.0.0.0:8000 --workers 3 swapapi.wsgi:application
