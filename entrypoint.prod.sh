95b105d5f8fc80a5569011514f46784b237465b3
unknown <olanrewajuadam2020@gmail.com>
2026-05-25 16:35:49 +0100

    worker reduced to 2

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -2,4 +2,4 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-exec gunicorn --bind 0.0.0.0:8000 --workers 3 swapapi.wsgi:application
+exec gunicorn --bind 0.0.0.0:8000 --workers 2 swapapi.wsgi:application
